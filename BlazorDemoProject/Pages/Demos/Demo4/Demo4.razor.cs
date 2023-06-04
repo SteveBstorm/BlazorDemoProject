8a858aecda2a7f0227e6510de8d8f2f51e7176f5
steve <steve.lorent@bstorm.be>
2023-06-04 20:31:29 +0200

    Ajout Demo Virtualize

---
+++ BlazorDemoProject/Pages/Demos/Demo4/Demo4.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorDemoProject.Pages.Demos.Demo4
+{
+    public partial class Demo4
+    {
+        public List<Item> ItemList { get; set; }
+        private int counter = 0;
+        public Demo4()
+        {
+            ItemList = new List<Item>();
+            for(int i = 0; i < 50; i++)
+            {
+                ItemList.Add(new Item
+                {
+                    Id = ++counter,
+                    Content = Guid.NewGuid().ToString(),
+                });
+            }
+        }
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+    }
+}
